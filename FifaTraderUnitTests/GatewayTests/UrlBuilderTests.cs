24b4b7f022600da80df88f02411ce00c2a3e48d8
James Martin <j.david.martin@hotmail.com>
2022-09-22 13:07:13 +0100

    updated tests to account for new urls

--- FifaTraderUnitTests/GatewayTests/UrlBuilderTests.cs
+++ FifaTraderUnitTests/GatewayTests/UrlBuilderTests.cs
@@ -1,10 +1,8 @@
 ﻿using FifaTrader.APIHandler.HttpHandlers.UrlBuilder;
 using FifaTrader.APIHandler.Interfaces;
 using FifaTrader.Models.EnvVariables;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using NUnit.Framework;
-using System.Collections.Generic;
 
 namespace FifaTraderUnitTests.GatewayTests
 {
@@ -29,7 +27,7 @@ namespace FifaTraderUnitTests.GatewayTests
 			// Arrange
 			var bidPrice = 1400;
 			var playedId = 1234;
-			var expected = $"https://utas.external.s2.fut.ea.com/ut/game/fifa22/transfermarket?type=player&maskedDefId=1234&macr=1300&num=21&start=0";
+			var expected = $"https://utas.mob.v1.fut.ea.com/ut/game/fifa22/transfermarket?type=player&maskedDefId=1234&macr=1300&num=21&start=0";
 
 			// Act
 			var actual = _urlBuilder.BuildSearchUrl(playedId, bidPrice);
@@ -44,7 +42,7 @@ namespace FifaTraderUnitTests.GatewayTests
 			// Arrange
 			var bidPrice = 900;
 			var playedId = 1234;
-			var expected = "https://utas.external.s2.fut.ea.com/ut/game/fifa22/transfermarket?type=player&maskedDefId=1234&macr=850&num=21&start=0";
+			var expected = "https://utas.mob.v1.fut.ea.com/ut/game/fifa22/transfermarket?type=player&maskedDefId=1234&macr=850&num=21&start=0";
 
 			// Act
 			var actual = _urlBuilder.BuildSearchUrl(playedId, bidPrice);
@@ -58,7 +56,7 @@ namespace FifaTraderUnitTests.GatewayTests
 		{
 			//Arrange
 			var tradeId = "12345";
-			var expected = "https://utas.external.s2.fut.ea.com/ut/game/fifa22/trade/12345/bid";
+			var expected = "https://utas.mob.v1.fut.ea.com/ut/game/fifa22/trade/12345/bid";
 
 			//Act
 			var actual = _urlBuilder.BuildBidUrl(tradeId);
@@ -72,7 +70,7 @@ namespace FifaTraderUnitTests.GatewayTests
 		{
 			//Arrange
 			var tradeIds = "12345,54321";
-			var expected = "https://utas.external.s2.fut.ea.com/ut/game/fifa22/watchlist?tradeId=12345,54321";
+			var expected = "https://utas.mob.v1.fut.ea.com/ut/game/fifa22/watchlist?tradeId=12345,54321";
 
 			//Act
 			var actual = _urlBuilder.BuildDeletePlayerUrl(tradeIds);
